27c515852f309affb1942c9e590261e675b4a07a
veja947 <veja947@gmail.com>
2021-02-18 22:37:21 -0500

    bring init value in UserInput, and remember, methods using arrow functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserOutput from './Components/UserOutput/UserOutput';
 import UserInput from "./Components/UserInput/UserInput";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <br/>
-        <UserOutput username='user name1'/>
-        <UserOutput username='user name2'/>
-      </div>
-    );
-  }
+    state = {
+        outputs: [
+            {
+                username: 'username 1 in state'
+            },
+            {
+                username: 'username 2 in state'
+            }
+        ]
+    };
+
+    changeUsername = () => {
+        this.setState({
+            outputs: [
+                {
+                    username: 'change username 1 in state'
+                },
+                {
+                    username: 'change username 2 in state'
+                }
+            ]
+        })
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <br/>
+                <button onClick={this.changeUsername}>Change Username</button>
+                <h1>user input 1</h1>
+                <UserInput currentValue = {this.state.outputs[0].username} />
+                <UserOutput username={this.state.outputs[0].username}/>
+                <h1>user input 2</h1>
+                <UserInput currentValue={this.state.outputs[1].username} />
+                <UserOutput username={this.state.outputs[1].username}/>
+            </div>
+        );
+    }
 }
 
 export default App;
